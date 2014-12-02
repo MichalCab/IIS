676ca873b6b38bb6853e36dd769f6cfa8b16c27a
fossilmfc <fossilmfc@gmail.com>
2014-12-03 00:00:59 +0100

    pridane zakladne info v home

--- application/views/home.php
+++ application/views/home.php
@@ -5,14 +5,26 @@
 		</div>
 	</div>
 	<div class="title">
-		GOvno
+		
 	</div>
 	<div class="modules_right">
 	</div>
 </div>
 <div id="desc">
 	<div class="body">
-	    
+	    <div class="box">
+            <div class="head"><div></div></div>
+            <h2>Informácie o užívateľovy</h2>
+            <div class="desc">
+                <div class="col w1"><div class="content"><h3>Meno:</h3></div></div>
+                <div class="col w1 last"><div class="content"><?php echo $user->meno; ?></div></div>
+                <div class="clear"></div>
+                <div class="col w1"><div class="content"><h3>Priezvisko:</h3></div></div>
+                <div class="col w1 last"><div class="content"><?php echo $user->priezvisko; ?></div></div>
+                <div class="clear"></div>
+            </div>
+            <div class="bottom"><div></div></div>
+        </div>
     </div>
 	<div class="clear"></div>
 	<div id="body_footer">
