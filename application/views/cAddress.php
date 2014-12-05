9025adc8a8afc353af244777d624b8055297f9a5
fossilmfc <fossilmfc@gmail.com>
2014-12-05 10:52:15 +0100

    updated model, fixed popup warning notify messages

--- application/views/cAddress.php
+++ application/views/cAddress.php
@@ -10,15 +10,21 @@
 					<th>Adresa</th>
 					<th></th>
 				</tr>
-				<?php foreach ($addresses as $item): ?>
-    				<tr id="<?php echo $item->id; ?>">
-    					<td><?php echo $item->adresa; ?></td>
-    					<td>
-    					   <a href="/<?php echo $this->router->class; ?>/delete" ajax-id="<?php echo $item->id; ?>" class="button ajax mRight"><small class="icon cross"></small><span>Zmazať</span></a>
-    					   <a href="/<?php echo $this->router->class; ?>/edit/<?php echo $item->id; ?>" class="button mRight"><small class="icon pencil"></small><span>Upraviť</span></a>
-    					</td>
+				<?php if(!empty($addresses)): ?>
+				    <?php foreach ($addresses as $item): ?>
+        				<tr id="<?php echo $item->id; ?>">
+        					<td><?php echo $item->adresa; ?></td>
+        					<td>
+        					   <a href="/<?php echo $this->router->class; ?>/delete" ajax-id="<?php echo $item->id; ?>" ajax-action="delete" class="button ajax mRight"><small class="icon cross"></small><span>Zmazať</span></a>
+        					   <a href="/<?php echo $this->router->class; ?>/edit/<?php echo $item->id; ?>" class="button mRight"><small class="icon pencil"></small><span>Upraviť</span></a>
+        					</td>
+        				</tr>
+    				<?php endforeach; ?>
+				<?php else: ?>
+				    <tr>
+    					<td colspan="2">Nemáte žiadnu adresu</td>
     				</tr>
-				<?php endforeach; ?>
+				<?php endif;?>
 			</tbody></table>
 		</div>							
 	</div>
