10f87849d4fe088219b9db437d1b6e3544c7b425
fossilmfc <fossilmfc@gmail.com>
2014-12-05 23:09:37 +0100

    produkt view

--- application/views/mProductDetail.php
+++ application/views/mProductDetail.php
@@ -1,22 +1,22 @@
-<h1>Informácie o objednávke</h1>
+<h1>Informácie o produkte</h1>
 <div class="box">
     <div class="head"><div></div></div>
     <div class="desc">
         <div class="col w1"><div class="content"><h3>Názov:</h3></div></div>
-        <div class="col w1 last"><div class="content"><?php echo $order->cislo; ?></div></div>
+        <div class="col w1 last"><div class="content"><?php echo $product->nazov; ?></div></div>
         <div class="clear"></div>
         <div class="col w1"><div class="content"><h3>Popis:</h3></div></div>
-        <div class="col w1 last"><div class="content"><?php echo $order->termin; ?></div></div>
+        <div class="col w1 last"><div class="content"><?php echo $product->popis; ?></div></div>
         <div class="clear"></div>
         <div class="col w1"><div class="content"><h3>Cena:</h3></div></div>
-        <div class="col w1 last"><div class="content"><?php echo $order->suma; ?></div></div>
+        <div class="col w1 last"><div class="content"><?php echo $product->cena; ?></div></div>
         <div class="clear"></div>
         <div class="col w1"><div class="content"><h3>Povolené:</h3></div></div>
         <div class="col w1 last"><div class="content">
-            <?php if($order->vybavene == 0): ?>
-                <a href="/<?php echo $this->router->class; ?>/set" ajax-id="<?php echo $item->id; ?>" ajax-action="stateChange" ajax-noanimation="true" class="button ajax mRight"><small class="icon check"></small><span>Povoliť</span></a>
+            <?php if($product->povolene == 0): ?>
+                <a href="/<?php echo $this->router->class; ?>/set/<?php echo $product->id; ?>/1" class="button"><small class="icon check"></small><span>Povoliť</span></a>
             <?php else: ?>
-                <a href="/<?php echo $this->router->class; ?>/set" ajax-id="<?php echo $item->id; ?>" ajax-action="stateChange" ajax-noanimation="true" class="button ajax mRight"><small class="icon cross"></small><span>Zakázať</span></a>
+                <a href="/<?php echo $this->router->class; ?>/set/<?php echo $product->id; ?>/0" class="button"><small class="icon cross"></small><span>Zakázať</span></a>
             <?php endif; ?>
             </div></div>
         <div class="clear"></div>
@@ -32,7 +32,7 @@
 					<th>Množstvo</th>
 					<th>Popis</th>
 				</tr>
-				<?php foreach ($product_materials as $item): ?>
+				<?php foreach ($product_material as $item): ?>
     				<tr>
     				    <td><?php echo $item->nazov; ?></td>
     					<td><?php echo $item->nakupnaCena; ?></td>
